9843f8cc5939e5da1318769f9bbe9a4734faabe7
criofox <79754985+criofox@users.noreply.github.com>
2021-06-20 14:38:42 +0300

    Add files via upload

--- Lesson_2/script.js
+++ Lesson_2/script.js
@@ -1,39 +1,38 @@
 /////////////// Задание 1
 
-const arrayForFill = []
+const arrayForFill = [];
 
 while (arrayForFill.length < 7) {
-    const randomNumber = Math.floor(Math.random() * 10)
+    const randomNumber = Math.floor(Math.random() * 10);
 
     if (!arrayForFill.includes(randomNumber)) {
         arrayForFill.push((randomNumber))
     }
 }
 
-console.log(arrayForFill)
-arrayForFill.sort()
-console.log(arrayForFill)
-
+console.log(arrayForFill);
+arrayForFill.sort();
+console.log(arrayForFill);
 
 /////////////// Задание 2
 
 
-const arrayForChange = []
+const arrayForChange = [];
 
 while (arrayForChange.length < 10) {
-    const randomNumber = Math.floor(Math.random() * 100)
+    const randomNumber = Math.floor(Math.random() * 100);
 
     if (!arrayForChange.includes(randomNumber)) {
         arrayForChange.push((randomNumber))
     }
 }
 
-console.log(arrayForChange)
+console.log(arrayForChange);
 
 arrayForChange.forEach((item, i) => {
     if (item % 10 === 0) {
-        arrayForChange[i] = arrayForChange[i].toString().replace('0', 'zero')
+        arrayForChange[i] = arrayForChange[i].toString().replaceAll('0', 'zero')
     }
-})
+});
 
-console.log(arrayForChange)
+console.log(arrayForChange);
